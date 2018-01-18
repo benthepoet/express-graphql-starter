67eedbb19623a2c5e0ac09158c651a92fdd2f035
Ben Hanna <benpaulhanna@gmail.com>
2018-01-18 17:34:37 +0000

    Added post type to the schema.

--- graphql/typedefs.js
+++ graphql/typedefs.js
@@ -1,8 +1,18 @@
 module.exports = `
+    input PostInput {
+        content: String
+    }
+
+    type Post {
+        content: String
+    }
+
     type Mutation {
+        createPost(postInput: PostInput): Post
     }
 
     type Query {
+        getPosts: [Post]
     }
 
     schema {
