7865c92ada566f159931d4372d02fdd5405ad409
Ben Hanna <benpaulhanna@gmail.com>
2018-01-18 18:31:38 +0000

    Began implementing password and refresh grant types.

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 
+const authRouter = require('./auth');
 const graphqlRouter = require('./graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+app.use(authRouter);
 app.use(graphqlRouter);
 
 app.listen(8020, () => console.log('API ready'));
--- auth/index.js
+++ auth/index.js
@@ -2,7 +2,56 @@ const jsonwebtoken = require('jsonwebtoken');
 const level = require('level');
 const { Router } = require('express');
 
+const GRANT = {
+    PASSWORD: 'password',
+    REFRESH: 'refresh'
+};
+
+const database = level('./tokendb');
+
 const router = Router();
 
+router.post('/auth/token', ({ body }, res) => {
+    switch (body.grantType) {
+        case GRANT.PASSWORD:
+            createRefreshToken()
+                .then(refreshToken => {
+                    res.send({
+                        accessToken: createAccessToken(),
+                        refreshToken
+                    });
+                }); 
+            break;
+        case GRANT.REFRESH:
+            validateRefreshToken(body.refreshToken)
+                .then(() => {
+                    res.send({
+                        accessToken: createAccessToken()
+                    });
+                });
+            break;
+        default:
+            res.sendStatus(400);
+            break;
+    }
+});
+
 module.exports = router;
+
+function createAccessToken() {
+    return jsonwebtoken.sign({}, 'a');
+}
+
+function createRefreshToken() {
+    const payload = {};
+    const refreshToken = jsonwebtoken.sign(payload, 'r');
+    return database
+        .put(refreshToken, payload)
+        .then(() => refreshToken);
+}
+
+function validateRefreshToken(refreshToken) {
+    return database
+        .get(refreshToken);
+}
 
